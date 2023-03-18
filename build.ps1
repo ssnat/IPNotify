6fb208097bd8cd2e6e346b026f5843e390dc7460
pengxiguaa <pengxiguaa@gmail.com>
2023-03-18 19:14:14 +0800

    pref: optimize build script

---
+++ build.ps1
@@ -0,0 +1,41 @@
+$version = $args[0]
+
+$dir = "./build/v$version"
+
+$linuxArm64Dir = $dir + "/IPNotify-linux-arm64"
+$linuxAmd64Dir = $dir + "/IPNotify-linux-amd64"
+$windowsArm64Dir = $dir + "/IPNotify-windows-arm64"
+$windowsAmd64Dir = $dir + "/IPNotify-windows-amd64"
+
+
+$linuxArm64Path = $linuxArm64Dir + "/IPNotify"
+$linuxAmd64Path = $linuxAmd64Dir + "/IPNotify"
+$windowsArm64Path = $windowsArm64Dir + "/IPNotify.exe"
+$windowsAmd64Path = $windowsAmd64Dir + "/IPNotify.exe"
+
+echo $linuxArm64Path
+$env:GOOS="linux"
+$env:GOARCH="arm64"
+go build -o $linuxArm64Path .
+
+echo $linuxAmd64Path
+$env:GOOS="linux"
+$env:GOARCH="amd64"
+go build -o $linuxAmd64Path .
+
+echo $windowsArm64Path
+$env:GOOS="windows"
+$env:GOARCH="arm64"
+go build -o $windowsArm64Path .
+
+echo $windowsAmd64Path
+$env:GOOS="windows"
+$env:GOARCH="amd64"
+go build -o $windowsAmd64Path .
+
+cp ./config.yaml $linuxArm64Dir
+cp ./config.yaml $linuxAmd64Dir
+cp ./config.yaml $windowsAmd64Dir
+cp ./config.yaml $windowsArm64Dir
+
+echo "Done"
